714b03caf845319ce813eb07f60fe8f142732de4
Mateus Santos do Nascimento <mateussantos1528@hotmail.com>
2024-03-08 12:32:00 -0300

    WIP: Quotes

--- mandatory/token/validations.c
+++ mandatory/token/validations.c
@@ -6,7 +6,7 @@
 /*   By: matesant <matesant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 17:30:22 by matesant          #+#    #+#             */
-/*   Updated: 2024/03/08 11:44:04 by matesant         ###   ########.fr       */
+/*   Updated: 2024/03/08 12:30:35 by matesant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,23 @@
 
 void	ft_clear_spaces(char **str)
 {
-	char	*start;
-	char	*end;
+	int	start;
+	int	end;
+	int	i;
 
-	start = *str;
-	end = *str + ft_strlen(*str);
-	while (ft_isspace(*start))
+	start = 0;
+	end = ft_strlen(*str) - 1;
+	while (ft_isspace((*str)[start]))
 		start++;
-	while (end > start && ft_isspace(*end))
+	while (end >= start && ft_isspace((*str)[end]))
 		end--;
-	(*end) = '\0';
-	ft_memmove(*str, start, end - start + 2);
+	i = start;
+	while (i <= end)
+	{
+		(*str)[i - start] = (*str)[i];
+		i++;
+	}
+	(*str)[i - start] = '\0';
 }
-void lalalal
+
+// void ft_skip_quotes()
