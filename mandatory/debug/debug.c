41bcee3f8f8e4c1765a81fb7f92ea2d6c3797ef4
Alisson Marcos <alissonmarcos249@gmail.com>
2024-03-12 08:56:12 -0300

    wip: print io struct

--- mandatory/debug/debug.c
+++ mandatory/debug/debug.c
@@ -24,6 +24,14 @@ static	void	print_argv(t_command *node)
 	}
 }
 
+static	void	print_io(t_command *node)
+{
+	printf("\t\t- infile: %s\n", node->io->infile);
+	printf("\t\t- outfile: %s\n", node->io->outfile);
+	printf("\t\t- infile_fd: %d\n", node->io->infile_fd);
+	printf("\t\t- outfile_fd: %d\n", node->io->outfile_fd);
+}
+
 void	print_cmd_list(t_command *list)
 {
 	printf("\n\n\n       ------ COMMAND LIST ------\n\n\n");
@@ -34,6 +42,9 @@ void	print_cmd_list(t_command *list)
 		if (list->argv)
 			print_argv(list);
 		printf("\t- Output to pipe: %d\n", list->output_to_pipe);
+		printf("\t- Io: %p\n", list->io);
+		if (list->io)
+			print_io(list);
 		printf("\t- Prev: %p\n", list->prev);
 		if (list->prev)
 			printf("\t\t- Name: %s\n", list->prev->name);
