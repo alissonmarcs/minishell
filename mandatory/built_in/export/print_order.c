9df7e90584f399dfce5177026dd669e05dc83602
mateus <mateussantos1528@hotmail.com>
2024-03-29 20:31:02 -0300

    correction on export

--- mandatory/built_in/export/print_order.c
+++ mandatory/built_in/export/print_order.c
@@ -6,7 +6,7 @@
 /*   By: matesant <matesant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 16:37:29 by matesant          #+#    #+#             */
-/*   Updated: 2024/03/29 20:16:37 by matesant         ###   ########.fr       */
+/*   Updated: 2024/03/29 20:30:08 by matesant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,11 @@ void	ft_print_in_order(char **env)
 			key = envl->key;
 			if (!ft_strcmp(env[i], key))
 			{
-				if (envl->value)
+				if (envl->value && ft_strlen(envl->value) > 0)
 					ft_printf_fd(STDOUT_FILENO, "declare -x %s=%s\n", envl->key,
 						envl->value);
+				else if (!envl->value)
+					ft_printf_fd(STDOUT_FILENO, "declare -x %s\n", envl->key);
 				else
 					ft_printf_fd(STDOUT_FILENO, "declare -x %s=\"\"\n", envl->key);
 				break ;
