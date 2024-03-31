337f5580be04fe58863ab7009bee8c2e17f27415
mateus <mateussantos1528@hotmail.com>
2024-03-30 23:37:01 -0300

    Exit corrections

--- mandatory/built_in/exit.c
+++ mandatory/built_in/exit.c
@@ -6,7 +6,7 @@
 /*   By: matesant <matesant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 11:36:00 by matesant          #+#    #+#             */
-/*   Updated: 2024/03/29 21:39:37 by matesant         ###   ########.fr       */
+/*   Updated: 2024/03/30 23:34:47 by matesant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,8 @@ void	ft_exit(char **argv)
 	if (ft_matrice_len(argv) > 2)
 		if(ft_exit_msg(argv, "too many arguments", NULL, 1))
 			return ;
-	exit(ft_get_shell()->exit_status);
+    if (argv[1])
+        ft_get_shell()->exit_status = ft_atoi(argv[1]);
+	
+    exit(ft_get_shell()->exit_status);
 }
