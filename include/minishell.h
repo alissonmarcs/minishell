603befb37890d37c0e71f9d25aada83cb947a423
Alisson <alissonmarcos249@gmail.com>
2024-04-05 15:38:34 -0300

    wip: put <stdio> above readline headers

--- include/minishell.h
+++ include/minishell.h
@@ -18,10 +18,10 @@
 # include "types.h"
 # include <errno.h>
 # include <fcntl.h>
+# include <stdio.h>
 # include <readline/history.h>
 # include <readline/readline.h>
 # include <signal.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
 # include <sys/stat.h>
