4b75d89cce695acce4d29d3b3cafa05b453e1d92
Mateus Santos do Nascimento <mateussantos1528@hotmail.com>
2024-04-16 17:43:21 -0300

    Pwd error implementation

--- mandatory/built_in/pwd.c
+++ mandatory/built_in/pwd.c
@@ -6,7 +6,7 @@
 /*   By: matesant <matesant@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 15:31:34 by matesant          #+#    #+#             */
-/*   Updated: 2024/04/10 13:54:39 by matesant         ###   ########.fr       */
+/*   Updated: 2024/04/16 17:42:26 by matesant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,15 @@ void	ft_pwd_builtin(char **argv)
 {
 	char	*pwd;
 
+	if (argv[1])
+	{
+		if (argv[1][0] == '-')
+		{
+			ft_printf_fd(STDERR_FILENO, "pwd: %s: invalid option\n", argv[1]);
+			return ;
+		}
+	}
 	ft_get_shell()->exit_status = 0;
-	(void)argv;
 	pwd = getcwd(NULL, 0);
 	ft_printf_fd(STDOUT_FILENO, "%s\n", pwd);
 	free(pwd);
