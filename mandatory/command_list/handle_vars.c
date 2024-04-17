1427cc77d1bf15485fa16baa456cdbf53a9c1b23
Alisson Marcos <alissonmarcos249@gmail.com>
2024-04-17 17:00:38 -0300

    wip: mandatory done

--- mandatory/command_list/handle_vars.c
+++ mandatory/command_list/handle_vars.c
@@ -12,6 +12,22 @@
 
 #include "minishell.h"
 
+static void	update_argv(t_command *last, char **split);
+static void	export(t_command *last, t_token *token);
+static void	others_cases(t_token *tokens, t_command *last);
+
+void	handle_vars(t_token **tokens, t_command *last)
+{
+	if ((*tokens)->str && (*tokens)->str[0])
+	{
+		if (ft_strcmp(last->name, "export") == 0)
+			export(last, *tokens);
+		else
+			others_cases(*tokens, last);
+	}
+	*tokens = (*tokens)->next;
+}
+
 static void	update_argv(t_command *last, char **split)
 {
 	char	**new_argv;
@@ -28,28 +44,48 @@ static void	update_argv(t_command *last, char **split)
 	last->argv = new_argv;
 }
 
-void	handle_vars(t_token **tokens, t_command *last)
+static void	export(t_command *last, t_token *token)
+{
+	char	**new_argv;
+	int		len;
+
+	if (!last->argv)
+	{
+		last->argv = ft_calloc(3, sizeof(char *));
+		last->argv[0] = ft_strdup(last->name);
+		last->argv[1] = ft_strdup(token->str);
+	}
+	else
+	{
+		len = ft_matrice_len(last->argv);
+		new_argv = ft_calloc(len + 2, sizeof(char *));
+		ft_memmove(new_argv, last->argv, len * sizeof(char *));
+		new_argv[len] = ft_strdup(token->str);
+		free(last->argv);
+		last->argv = new_argv;
+	}
+	print_cmd_list(last);
+}
+
+static void	others_cases(t_token *tokens, t_command *last)
 {
 	char	**split;
 
-	if ((*tokens)->str && (*tokens)->str[0])
+	split = ft_split(tokens->str, ' ');
+	if (!last->name && !last->argv)
 	{
-		split = ft_split((*tokens)->str, ' ');
-		if (!last->name && !last->argv)
-		{
-			last->name = ft_strdup(split[0]);
-			last->argv = split;
-		}
-		else if (last->name && last->argv)
-			update_argv(last, split);
-		else if (last->name && !last->argv)
-		{
-			last->argv = ft_calloc(ft_matrice_len(split) + 2, sizeof(char *));
-			last->argv[0] = ft_strdup(last->name);
-			ft_memmove(last->argv + 1, split, ft_matrice_len(split)
-				* sizeof(char *));
-			free(split);
-		}
+		last->name = ft_strdup(split[0]);
+		last->argv = split;
 	}
-	*tokens = (*tokens)->next;
+	else if (last->name && last->argv)
+		update_argv(last, split);
+	else if (last->name && !last->argv)
+	{
+		last->argv = ft_calloc(ft_matrice_len(split) + 2,
+				sizeof(char *));
+		last->argv[0] = ft_strdup(last->name);
+		ft_memmove(last->argv + 1, split, ft_matrice_len(split)
+			* sizeof(char *));
+		free(split);
+	}
 }
